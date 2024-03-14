a076304d91877244337377709980265dfd56d05d
SeonJK <tjswhdrbs14@naver.com>
2024-03-14 19:50:29 +0900

    add landing navigation group

--- app/src/main/java/com/seonjk/smartdeliveryclone/navigation/AppRouter.kt
+++ app/src/main/java/com/seonjk/smartdeliveryclone/navigation/AppRouter.kt
@@ -4,12 +4,23 @@ package com.seonjk.smartdeliveryclone.navigation
 
 private object Route {
     const val HOME = "home"
+
+    // LANDING Group
     const val LANDING = "landing"
+    const val SERVICE_AGREEMENT = "service_agreement"
+    const val PHONE_AUTHENTICATION = "phone_authentication"
+
     const val MAIN = "main"
 }
 
 sealed class Screen(val route: String) {
     object Home: Screen(Route.HOME)
-    object Landing: Screen(Route.LANDING)
     object Main: Screen(Route.MAIN)
-}
+}    // LANDING Group
+    object Landing: Screen(Route.LANDING) {
+        // 이용약관
+        object ServiceAgreement: Screen(Route.SERVICE_AGREEMENT)
+        // 번호인증
+        object PhoneAuthentication: Screen(Route.PHONE_AUTHENTICATION)
+    }
+    // MAIN Group
