2e90c18df42f6c0a6ce7687a0e46d07ee771f434
SeonJK <tjswhdrbs14@naver.com>
2024-05-29 12:59:02 +0900

    delete unused imports

--- app/src/main/java/com/seonjk/smartdeliveryclone/ui/components/common/SdcTextField.kt
+++ app/src/main/java/com/seonjk/smartdeliveryclone/ui/components/common/SdcTextField.kt
@@ -1,21 +1,14 @@
 package com.seonjk.smartdeliveryclone.ui.components.common
 
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.seonjk.smartdeliveryclone.ui.theme.SmartDeliveryCloneTheme
